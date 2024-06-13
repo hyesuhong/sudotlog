bbfd5fbbfe051047f1e70c0aa4ba66123e0a5e26
Hyesu <honghs95@gmail.com>
2024-06-13 12:23:08 +0900

    feat: separate about data from page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,5 @@
+import { Image } from '@/components/post/elements';
+import data from '@/contents/about.json';
 import {
 	aboutFocusText,
 	aboutImageWrapper,
@@ -18,61 +20,27 @@ export default function Page() {
 				<h1 className={postInfoTitle}>
 					About <span className={aboutFocusText}>Su</span>
 				</h1>
-				<div className={aboutImageWrapper}>{/* img wrapper */}</div>
-				<p className={postContentPara}>
-					안녕하세요, 프론트엔드 개발자 Su입니다.
-				</p>
+				<div className={aboutImageWrapper}>{data.profile && <Image />}</div>
+				<p className={postContentPara}>{data.description}</p>
 			</section>
 			<section className={aboutSection}>
 				<h2 className={postContentHeadings({ kind: 'heading1' })}>
 					Let&apos;s get in touch
 				</h2>
 				<ul className={postContentUnorderd}>
-					<li>
-						<Link
-							href='mailto:honghs95@gmail.com'
-							target='_blank'
-							className={postContentLink}
-						>
-							Mail
-						</Link>
-					</li>
-					<li>
-						<Link
-							href='https://github.com/hyesuhong'
-							target='_blank'
-							className={postContentLink}
-						>
-							Github
-						</Link>
-					</li>
-					<li>
-						<Link
-							href='https://www.linkedin.com/in/hyesu-hong-03110725a'
-							target='_blank'
-							className={postContentLink}
-						>
-							Linkedin
-						</Link>
-					</li>
+					{data.contacts.map((contact, index) => (
+						<li key={index}>
+							<Link
+								href={contact.href}
+								target='_blank'
+								className={postContentLink}
+							>
+								{contact.name}
+							</Link>
+						</li>
+					))}
 				</ul>
 			</section>
-			{/* <section className={section}>
-				<h2 className={postContentHeadings({ kind: 'heading1' })}>
-					More about experiences
-				</h2>
-				<ul className={postContentunOrderd}>
-					<li>
-						<Link
-							href=''
-							target='_blank'
-							className={postContentLink({ kind: 'outer' })}
-						>
-							Notion Resume
-						</Link>
-					</li>
-				</ul>
-			</section> */}
 		</main>
 	);
 }
