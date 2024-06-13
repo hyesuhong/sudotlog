6648f3dd0271960c1618c74ced39d6a03b3b721c
Hyesu <honghs95@gmail.com>
2024-06-13 13:44:37 +0900

    feat: set root metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,36 @@
 import { Footer } from '@/components/footer';
 import { Header } from '@/components/header';
 import { ThemeProvider } from '@/components/theme';
+import meta from '@/contents/metadata.json';
 import { rootClassName } from '@/styles';
 import { jetBrainsMono, pretendard } from '@/utils/font';
 import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-	title: '',
-	description: '',
+	/* TODO: set metadataBase to use og & twitter image */
+	title: meta.title,
+	description: meta.description,
+	keywords: meta.keywords,
+	authors: meta.authors,
+	icons: meta.icons,
+	openGraph: {
+		title: meta.title,
+		description: meta.description,
+		type: 'website',
+		siteName: meta.title,
+		images: {
+			url: meta.og.image,
+		},
+	},
+	twitter: {
+		title: meta.title,
+		description: meta.description,
+		card: 'summary_large_image',
+		images: {
+			url: meta.twitter.image,
+		},
+	},
 };
 
 export default function RootLayout({
